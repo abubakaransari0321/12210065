67dc3f2ab141cb50f0201852ea9b9db015e69a58
abubakaransari0321 <118571703+abubakaransari0321@users.noreply.github.com>
2025-07-14 12:33:21 +0530

    Create server.js

---
+++ Backend Test Submission/server.js
@@ -0,0 +1,12 @@
+const app = require("./app");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+  })
+  .catch((err) => console.error("MongoDB connection error", err));
